c2576a17a37af173ce635268de4173a2d0dca181
Giselle Smith <gisellec60@gmail.com>
2023-01-26 15:36:14 -0500

    index.html and sytle.css 1/26/2023

--- index.js
+++ index.js
@@ -16,8 +16,27 @@ linkedIn.addEventListener('mouseover', function () {
     linkedIn.innerHTML = "https://www.linkedin.com/in/giselle-smith-57baaa5a/";
 });
 
-linkedIn.addEventListener("mouseout", function () { 
+linkedIn.addEventListener("mouseout", function () {
     linkedIn.innerHTML = linkedInOrig;
-})
+});
+//Visitor Code
+let counterContainer = document.querySelector(".website-counter");
+let resetButton = document.querySelector("#reset");
+let visitCount = localStorage.getItem("page_view");
 
- 
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+// Adding onClick event listener
+resetButton.addEventListener("click", () => {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+  counterContainer.innerHTML = visitCount;
+});
